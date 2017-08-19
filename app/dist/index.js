edf64db244e6f703dcfda6b1f3fa9faaf64e96b6
Fernando <lordy.vox@gmail.com>
2017-08-19 17:01:30 -0500

    START

---
+++ app/dist/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component() {
+
+	constructor() {
+		super();
+		this.nombre = "Angel Fernando";
+	}
+
+	render() {
+		return(
+
+			<h1>Hola {this.nombre}</h1>
+		);
+	}
+
+
+}
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
